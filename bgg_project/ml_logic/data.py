d9a437b80a583338a576c3dc770ca66054371953
Maxence Dauphin <maxence.dauphin94@gmail.com>
2025-03-23 10:20:10 +0100

    Addition of the vectorizing_column function that vectorize a column into its components (applicable for mechanics and domains) + extraction of the new obtained columns

--- bgg_project/ml_logic/data.py
+++ bgg_project/ml_logic/data.py
@@ -1,9 +1,10 @@
 import re
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 from bgg_project.params import *
 
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_data(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Index, pd.Index]:
 
     rows, col = df.shape
     print(f"Before cleaning data, total rows : {rows}, total columns : {col}")
@@ -24,23 +25,62 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df[df['owned_users'].notna()]
 
     # Impute missing values for mechanics and domains "unspecified"
-    df["mechanics"] = df["mechanics"].fillna("unspecified")
-    df["domains"] = df["domains"].fillna("unspecified")
+    df["mechanics"] = df["mechanics"].fillna("unspecified mechanic")
+    df["domains"] = df["domains"].fillna("unspecified domain")
 
     # Rework mechanics and domains columns which contains a list of strings
-    def clean(text):
-        cln = re.sub('[^a-zA-Z], ,', ' ', str(text))
-        cln = cln.replace("/",",")
-        cln = cln.split()
-        cln = ' '.join(cln)
-        return cln
-
     df["mechanics"] = df["mechanics"].apply(clean)
     df["domains"] = df["domains"].apply(clean)
 
+    # NEW : vectorized mechanics and domains + their associated columns
+    df, mechanics_columns = vectorizing_column(df, 'mechanics')
+    df, domains_columns = vectorizing_column(df, 'domains')
     rows, col = df.shape
     print(f"After cleaning data, total rows : {rows}, total columns : {col}")
 
     print("✅ data cleaned")
 
-    return df
+    return df, mechanics_columns, domains_columns
+
+
+def clean(text):
+        """
+        This function aims to clean a list of words. "
+        It takes a string of words separated by a comma as an input,
+        and returns the same string, but without space or slashes inbetween words.
+        Ex :
+        - Before a clean function: Action Drafting, Area Majority / Influence, ...
+        - After a clean function: Action Drafting,Area Majority,Influence,...
+        """
+        # This part replace empty words between commas, and separate slashed words by a comma
+        cln = re.sub(', ,', ' ', str(text))
+        cln = cln.replace(" /",",")
+        cln = cln.replace("/",",")
+        #cln = cln.lower() # if it is case sensitive, might be good to lowercase.
+
+        # This part enable to separate and join again the slashed words, and to get rid of spaces after the comma
+        """
+        Ex : 'Action', 'Drafting,...', -> 'Action Drafting,...'
+        """
+        cln = cln.split()
+        cln = ' '.join(cln)
+        cln = cln.split(', ')
+        cln = ','.join(cln)
+        return cln
+
+
+def vectorizing_column(df: pd.DataFrame, col: str) -> tuple[pd.DataFrame, pd.Index]:
+        """
+        Explanations !
+        """
+        token_pattern = r"([^,]+)"
+        count_vectorizer = CountVectorizer(token_pattern = token_pattern)
+        cols = count_vectorizer.fit_transform(df[str(col)])
+        vectorized_cols = pd.DataFrame(cols.toarray(),
+                                       columns = count_vectorizer.get_feature_names_out()
+                                       )
+
+        new_df = df.copy().reset_index(drop=True)
+        new_df = pd.concat([new_df, vectorized_cols], axis = 1)
+        new_df.drop([str(col)], axis = 1, inplace = True)
+        return new_df, vectorized_cols.columns
