3949c7c237af2630132f33dcf6692cd11ae7d85b
Konstantin Shapovalov <shapovalovkv@mail.com>
2025-04-05 11:34:10 +0200

    update registry.py

--- bgg_project/ml_logic/registry.py
+++ bgg_project/ml_logic/registry.py
@@ -1,6 +1,7 @@
 import os
 import time
 import pickle
+import shutil
 
 from bgg_project.params import *
 
@@ -22,5 +23,17 @@ def save_model(model, model_type='model'):
         pickle.dump(model, file)
 
     print(f"✅ Model saved as {filename}")
+
+    # Create a copy with a fixed name for xgboost models
+    if model_type.lower() == 'xgboost':
+        optimised_path = os.path.join(LOCAL_MODELS_PATH, f"{model_type}_optimised.pkl")
+        shutil.copy(pickle_path, optimised_path)  # Copy the same file
+
+        # Check if the file existed before and notify replacement
+        if os.path.exists(optimised_path):
+            print(f"⚠️  {model_type}_optimised.pkl already existed and was replaced.")
+
+        print(f"✅ Model to be used in Streamlit, successfully updated and saved : {model_type}_optimised.pkl")
+
 
     return None
